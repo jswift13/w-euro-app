80b84f4f4a3ce9ee4f8587d9413452b556d1e0f8
renancrc99 <renancrc@hotmail.com>
2025-07-06 02:57:38 -0300

    Load .env in wsgi and default to prod settings

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,25 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import environ
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+# Build paths inside the project like this: BASE_DIR / ...
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# read .env file
+env = environ.Env()
+env_file = os.path.join(BASE_DIR, '.env')
+if os.path.exists(env_file):
+    environ.Env.read_env(env_file)
+
+# allow overriding via environment; default to prod settings
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    env('DJANGO_SETTINGS_MODULE', default='config.settings.prod')
+)
 
 application = get_wsgi_application()
+
+
+
